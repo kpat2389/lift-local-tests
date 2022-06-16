55ecd515419ca8feb1b7b36c1d939e27fb8ff4fe
KP4RU <patelkaran2389@gmail.com>
2022-06-16 16:54:34 -0400

    lift tool assignment

--- JsonValidator/src/JsonValidator.java
+++ JsonValidator/src/JsonValidator.java
@@ -1,10 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class JsonValidator {
+
+    private static boolean checkBrackets(Stack<String> storeBrackets, String[] splitInformation, Scanner sc) {
+        for (int i = 0; i < splitInformation.length; i++) {
+            if ((splitInformation[i].equals("{") || splitInformation[i].equals("["))) {
+                storeBrackets.push(splitInformation[i]);
+                if (splitInformation.length != (i + 1)) {
+                    return false;
+                }
+            }
+            if (splitInformation[i].equals("}") || splitInformation[i].equals("},")) {
+                if (storeBrackets.isEmpty() || !storeBrackets.pop().equals("{")) {
+                    return false;
+                }
+            }
+            if (splitInformation[i].equals("]") || splitInformation[i].equals("],")) {
+                if (storeBrackets.isEmpty() || !storeBrackets.pop().equals("[")) {
+                    return false;
+                }
+            }
+            if (splitInformation[i].equals("}") && (sc.hasNext() || splitInformation.length != (i + 1))) {
+                return false;
+            }
+            if ((splitInformation[i].equals("],") || splitInformation[i].equals("},")) && (!sc.hasNext() ||
+                    splitInformation.length != (i + 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     private static void version() {
         System.out.println(1);
@@ -14,33 +43,53 @@ public class JsonValidator {
         System.out.println("true");
     }
 
-    private static void run(Scanner sc) {
+    private static void run(Scanner sc, File file) {
+        boolean valid = true;
+        int line = 1;
+        ArrayList<String> brackets = new ArrayList<>();
+        brackets.add("["); brackets.add("{"); brackets.add("]"); brackets.add("}"); brackets.add("],"); brackets.add("},");
+
         Stack<String> storeBrackets = new Stack<>();
         while(sc.hasNext()) {
-            String[] splitInformation = sc.nextLine().split("\\s+");
-            for (int i = 0; i < splitInformation.length; i++) {
-                if ((splitInformation[i].equals("{") || splitInformation[i].equals("["))) {
-                    storeBrackets.push(splitInformation[i]);
-                    if (splitInformation.length != (i + 1)) {
-                        System.out.println("No");
-                        return;
-                    }
-                }
-                if (splitInformation[i].equals("}") || splitInformation[i].equals("},")) {
-                    if (storeBrackets.isEmpty() || !storeBrackets.pop().equals("{")) {
-                        System.out.println("No");
-                        return;
-                    }
-                }
-                if (splitInformation[i].equals("]") || splitInformation[i].equals("],")) {
-                    if (storeBrackets.isEmpty() || !storeBrackets.pop().equals("[")) {
-                        System.out.println("No");
-                        return;
-                    }
-                }
+            String[] splitInformation = sc.nextLine().replaceAll("(^\\s+|\\s+$)", "").split("\\s+");
+            System.out.println(splitInformation[0]);
+            if (splitInformation.length > 2 || (!brackets.contains(splitInformation[0]) && splitInformation[0].charAt(0)
+                    != '\"')) {
+                valid = false;
+                break;
             }
+            if (!checkBrackets(storeBrackets, splitInformation, sc)) {
+                valid = false;
+                break;
+            }
+            if (splitInformation[0].charAt(0) == '\"' && (splitInformation[0].charAt(splitInformation[0].length() - 2)
+                    != '\"' || splitInformation[0].charAt(splitInformation[0].length() - 1) != ':')) {
+                valid = false;
+                break;
+            }
+            String checker = splitInformation[splitInformation.length - 1];
+            if ((sc.hasNext("]") || sc.hasNext("],") || sc.hasNext("}") || sc.hasNext("},"))
+                    && checker.charAt(checker.length() - 1) != '\"') {
+                valid = false;
+                break;
+            }
+            line++;
         }
-        System.out.println("Yes");
+        if (!storeBrackets.isEmpty()) {
+            valid = false;
+        }
+        if (!valid) {
+            String output = "[ { \"type\": \"Invalid Json format\"\n";
+            output = output + "    \"message\": \"Your txt file is not in Json format\"\n";
+            output = output + "    \"file\": \"" + file.getPath() + "\"\n";
+            output = output + "    \"line\": " + line + "\n";
+            output = output + "    \"details_url\": null\n";
+            output = output + "  }\n";
+            output = output + "]\n";
+            System.out.println(output);
+        } else {
+            System.out.println("This txt file is correctly formatted.");
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -52,10 +101,10 @@ public class JsonValidator {
             } else if (command.equals("applicable")) {
                 applicable();
             } else if (command.equals("run")) {
-                System.out.println("Please entire the path to a valid txt file: ");
+                System.out.println("Please entire the path to a txt file: ");
                 File file = new File("JsonValidator/src/test.txt");
                 Scanner scFile = new Scanner(file);
-                run(scFile);
+                run(scFile, file);
             } else if (command.equals("q")) {
                 System.out.println("Program ended");
                 break;
